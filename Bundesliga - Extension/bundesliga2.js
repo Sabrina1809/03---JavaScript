b4a1544fc71ec1a8ae636b63d151a6e2eb036156
Sabrina Fritz <Sabrina_Fritz@gmx.net>
2024-05-13 20:35:01 +0200

    DOM Trommel

--- Bundesliga - Extension/bundesliga2.js
+++ Bundesliga - Extension/bundesliga2.js
@@ -1,85 +1,60 @@
 let time = document.getElementById("time");
 
 let now = new Date();
-// onsole.log(now);
 
 let jahr = now.getFullYear();
-// console.log(jahr);
 let mon = now.getMonth();
-// console.log(mon);
 let tag = now.getDate();
-// console.log(tag);
 let stunde = now.getHours();
-// console.log(stunde);
 let minute = now.getMinutes();
-// console.log(minute);
 let sek = now.getSeconds();
-// console.log(sek);
 
 switch (mon) {
     case 0:
         mon = "Jan";
-        // console.log(mon);
         break;
     case 1:
         mon = "Feb";
-        // console.log(mon);
         break;
     case 2:
         mon = "Mrz";
-        // console.log(mon);
         break;
     case 3:
         mon = "Apr";
-        // console.log(mon);
         break;
     case 4:
         mon = "Mai";
-        // console.log(mon);
         break;
     case 5:
         mon = "Jun";
-        // console.log(mon);
         break;
     case 6:
         mon = "Jul";
-        // console.log(mon);
         break;
     case 7:
         mon = "Aug";
-        // console.log(mon);
         break;
     case 8:
         mon = "Sep";
-        // console.log(mon);
         break;
     case 9:
         mon = "Okt";
-        // console.log(mon);
         break;
     case 10:
         mon = "Nov";
-        // console.log(mon);
         break;
     case 11:
         mon = "Dez";
-        // console.log(mon);
         break;
 }
 
 let zweistelligTag = String(tag).padStart(2, '0'); 
-// console.log(zweistelligTag);
-
 
 function umwandeln() {
     let tagXX = String(tag).padStart(2, '0');
     let stundeXX = String(stunde).padStart(2, '0');
     let minuteXX = String(minute).padStart(2, '0');
     let sekundeXX = String(sek).padStart(2, '0');
-    // console.log(tagXX);
-    // console.log(stundeXX);
-    // console.log(minuteXX);
-    // console.log(sekundeXX);
 
     let ueberschrift = 
 `${jahr}-${mon}-${tagXX}
@@ -94,24 +69,17 @@ umwandeln();
 
 
 
+let bl1api = 'https://api.openligadb.de/getmatchdata/bl1/2023/33'; 
+let bl2api = 'https://api.openligadb.de/getmatchdata/bl2/2023/33'; 
+let bl3api = 'https://api.openligadb.de/getmatchdata/bl3/2023/37'; 
 
-
-
-
-
-
-
-
-
-
-const team1 = "Offenburg";
-const team2 = "Unzhurst";
-const toreTeam1 = 5;
-const toreTeam2 = 1;
+let team1 = "Offenburg";
+let team2 = "Unzhurst";
+let toreTeam1 = 5;
+let toreTeam2 = 1;
 
 let team = (`${team1} vs ${team2}`).toString();
 let tore = (`${toreTeam1}:${toreTeam2}`).toString();
-
 
 const ul = document.querySelector('.list');  //ul
 
@@ -127,6 +95,75 @@ let pTore = document.createElement('p');
 pTore.className = "tore";
 pTore.innerText = tore;
 console.log(tore);
+
+function replaceData(data) {
+    team1.innerText = `${data[0].team1.teamName}`;
+    console.log(team1);
+    team2.innerText = `${data[0].team2.teamName}`;
+    console.log(team2);
+    //----------------------------------------------------
+    // toreTeam1.innerText = data[0].goals.scoreTeam1; //Tore werden nciht ausgegeben
+    // console.log(toreTeam1);   ///
+    // console.log(data);
+}
+
+
+function bl1() {
+    fetch(bl1api)   
+    .then((res) => res.json())
+    .then((data) => replaceData(data));
+    
+}
+// bl1();
+
+function bl2() {
+    fetch(bl2api)   
+    .then((res) => res.json())
+    .then((data) => replaceData(data));
+    
+}
+// bl2();
+
+function bl3() {
+    fetch(bl3api)   
+    .then((res) => res.json())
+    .then((data) => replaceData(data));
+    
+}
+// bl3();
+
+
+let ligaInput = document.querySelector('#ligaInput');
+
+ligaInput.addEventListener('keydown', function (e) {
+    if (e.key === "Enter") {
+        if (ligaInput.value === "bl1") {
+            function getData() {
+                fetch(bl1api)   
+                .then((res) => res.json())
+                .then((data) => console.log(data));
+            }
+            getData();
+        } else if (ligaInput.value === "bl2") {
+            function getData() {
+                fetch(bl2api)   
+                .then((res) => res.json())
+                .then((data) => console.log(data));
+            }
+            getData();
+        } else if (ligaInput.value === "bl3") {
+            function getData() {
+                fetch(bl3api)   
+                .then((res) => res.json())
+                .then((data) => console.log(data));
+            }
+            getData();
+        } else {
+            console.log("Eingabe überprüfen!");
+        }
+    }
+});
+
 
 
 li.appendChild(pTeam);
