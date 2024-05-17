c37f42fe2c3fdd4f485fcd0e4086909afa597163
Sabrina Fritz <Sabrina_Fritz@gmx.net>
2024-05-17 20:58:32 +0200

    Wetter ohne Interval, ohne Icon

--- 6 - Wetterapp/script.js
+++ 6 - Wetterapp/script.js
@@ -1,4 +1,32 @@
-const city = "Düsseldorf" // Anpassen für die eigene Stadt
-
+const city = "Hurghada" // Anpassen für die eigene Stadt
 document.querySelector(".city").innerText = city;
 
+
+
+function getData(city) {
+    fetch(`http://api.weatherapi.com/v1/current.json?key=cfc91b5b2c4a443baf375330240905&q=${city}&aqi=no`
+)   
+    .then((res) => res.json())
+    .then((data) => replaceData(data));
+}
+
+
+
+function replaceData(data) {
+    console.log(data);
+
+    const temperature = data.current.temp_c;
+    document.querySelector(".temperature").innerText = temperature;
+
+    const img = `https:${data.current.condition.icon}`;
+    document.querySelector(".img").textContent = img;
+
+    const update = data.current.last_updated;
+    document.querySelector(".time").innerText = update;
+
+    console.log("10 Sekunden vorbei");
+
+    
+
+}
+getData(city)
